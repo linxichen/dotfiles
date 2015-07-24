bd383537b272864588965ab4ba4efa6571cd8ad6
linxichen <linxichen88@gmail.com>
2015-07-24 12:57:33 -0400

    updated redhat installation scripts. remove the powerline font part b/c i dont need it.

--- install/install_redhat.sh
+++ install/install_redhat.sh
@@ -1,14 +1,7 @@
 #! /bin/bash
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf ~/GitHub/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/GitHub/dotfiles/vim/.vimrc ~/.vimrc
-
-# Download powerline fonts but use them in vim-airline
-echo "installing powerline fonts..."
-git clone https://github.com/powerline/fonts.git
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
 
 # run vim and install plugins
 vim +PluginInstall +q +q
