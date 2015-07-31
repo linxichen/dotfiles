b3e9ac55accbdf4ebb6c188b64a8f8a9b3d03ea1
linxichen <linxichen88@gmail.com>
2015-07-31 09:13:04 -0400

    changed invisibility of dot files. also make some incomplete changes for k40

--- install/install_redhat.sh
+++ install/install_redhat.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf ~/GitHub/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/GitHub/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/GitHub/dotfiles/vim/dotvimrc_k40 ~/.vimrc
 
 # run vim and install plugins
 vim +PluginInstall +q +q
