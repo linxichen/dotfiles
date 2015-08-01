e5cc581afad0f4e219eea8a5bc4cc5d69020a083
linxichen <linxichen88@gmail.com>
2015-07-31 23:08:31 -0400

    fixed typos in installation scripts

--- install/install_redhat.sh
+++ install/install_redhat.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
+# install libevent 2.0
+cd /tmp
+wget https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-2.0.22-stable.tar.gz
+tar -xvzf libevent-2.0.22-stable.tar.gz
+cd libevent-2.0.22-stable
+./configure
+make
+sudo make install
+
+# update tmux first?
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
+
+# need to update ldpath so tmux can find libevent
+echo " echo '/usr/local/bin' >> /etc/ld.so.conf.d/newtmux2.conf " | sudo sh
+
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -sf ~/GitHub/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/GitHub/dotfiles/tmux/dottmuxdotconf ~/.tmux.conf
 ln -sf ~/GitHub/dotfiles/vim/dotvimrc_k40 ~/.vimrc
 
 # run vim and install plugins
--- install/install_ubuntu.sh
+++ install/install_ubuntu.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf ~/GitHub/dotfiles/tmux/dottmuxdotconf ~/.tmux.conf
-ln -sf ~/GitHub/dotfiles/vim/dotvimrc_k40 ~/.vimrc
+ln -sf ~/GitHub/dotfiles/vim/dotvimrc ~/.vimrc
 
 # run vim and install plugins
 vim +PluginInstall +q +q
