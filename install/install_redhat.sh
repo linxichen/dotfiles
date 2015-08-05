32243b87a26da6c1b42c0a55f570e75595bff982
linxichen <linxichen88@gmail.com>
2015-08-05 11:46:48 -0400

    uses common vimrc, not single k40 one. also added cp profile.d into redhat install scripts

--- install/install_redhat.sh
+++ install/install_redhat.sh
@@ -19,10 +19,13 @@ echo " echo '/usr/local/bin' >> /etc/ld.so.conf.d/newtmux2.conf " | sudo sh
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -sf ~/GitHub/dotfiles/tmux/dottmuxdotconf ~/.tmux.conf
-ln -sf ~/GitHub/dotfiles/vim/dotvimrc_k40 ~/.vimrc
+ln -sf ~/GitHub/dotfiles/vim/dotvimrc ~/.vimrc
 
 # run vim and install plugins
 vim +PluginInstall +q +q
 
 # Download Base16 shell so color is right even in tmux
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+# Cp all scripts to /etc/profile.d
+sudo cp ../ubuntu_profile_d/*.sh /etc/profile.d/
