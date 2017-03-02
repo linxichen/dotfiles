f607a151ea52e769bc0b8414fff28d5a784727b0
Linxi Chen <linxichen88@mgail.com>
2017-03-02 00:36:42 -0500

    added script that connects to wifi on MBA

---
+++ scripts/mba_wifi.sh
@@ -0,0 +1,3 @@
+# iwconfig wlx9cefd5fe9d95 essid "Bean Traders Free WIFI 2.4GHZ" enc s:"pieordie"
+# dhclient wlx9cefd5fe9d95
+nmcli device wifi connect "Bean Traders Free WIFI 2.4GHz" password "pieordie"
