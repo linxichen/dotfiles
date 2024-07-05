4fc2a68c9a59cc372e754ad44d88f72786c309d2
Linxi Chen <linxichen88@gmail.com>
2024-07-05 03:03:27 -0400

    add wezterm config but it's kinda slow

---
+++ wezterm/dotwezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Frappe"
+-- for i3 usage, cleaner
+config.window_background_opacity = 0.95
+config.enable_tab_bar = false -- remove tab bar
+config.window_decorations = "RESIZE" -- this removes title bar
+-- remove paddings to remove odd border
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+config.front_end = "OpenGL"
+
+-- and finally, return the configuration to wezterm
+return config
